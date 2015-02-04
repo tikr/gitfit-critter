b5827224c6a4183e1543288da5b919cd0304c8e6
Richard VanBreemen <rvbsanjose@me.com>
2015-02-04 10:10:07 -0800

    Cleaning up file structure

--- app.js
+++ app.js
@@ -2,36 +2,21 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-
-var routes = require('./routes/index');
-var session = require('express-session');
+var users = require('./routes/users');
 
 var app = express();
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/progress',express.static(__dirname + '/./public'));
-app.use('/achievements',express.static(__dirname + '/./public'));
-app.use(express.static(__dirname + '/./public'));
+app.use(express.static(path.join(__dirname, '/../public')));
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
+app.use('/api/users', users);
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.use('/', routes);
-app.use('/auth/fitbit/callback', routes);
 
+console.log('Express server started');
 module.exports = app;
-console.log('Express listening on: ');
