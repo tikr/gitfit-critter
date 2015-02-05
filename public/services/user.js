c8bf4d14c6900d7ea9ad0d47dcee600e05eab8d1
Richard VanBreemen <rvbsanjose@me.com>
2015-02-05 14:07:17 -0800

    Updating the critter profiles

--- public/services/user.js
+++ public/services/user.js
@@ -61,52 +61,50 @@ angular.module('gitfitApp.service.user', [])
       schedule: function (user) {
         var plots = [];
         var patts = [];
-        var patterns = [
-          '#EFC94C', '#45B29D', '#E27A3F', '#DF5A49', '#FF436A', '#13DCF2',
-          '#06ADBF', '#B9E366', '#7D4F7D', '#BBBEB5', '#9DA5A8', '#757E8E',
-          '#F2E3B6', '#F28C8C', '#BEDB39', '#1C8442', '#6F92BF', '#B0D165'
-        ];
-        var langs = ['JavaScript', 'Ruby', 'Python', 'C++', 'Swift'];
-        for (var i = 0; i < langs.length; i++) {
-          var points = [langs[i]];
-          patts.push(patterns[Math.floor(Math.random() * patterns.length)]);
-          for (var j = 0; j < 24; j++) {
-            points.push(Math.floor(Math.random() * 30));
+        var colors = $http({
+          method: 'GET',
+          url: '/api/languages'
+        }).then(function (data) {
+          var langs = ['JavaScript', 'Ruby', 'Python', 'C++', 'PowerShell', 'Rust', 'Pascal', 'Swift'];
+          for (var i = 0; i < langs.length; i++) {
+            var points = [langs[i]];
+            var pat = data.data[langs[i]];
+            patts.push(pat.color);
+            for (var j = 0; j < 24; j++) {
+              points.push(Math.floor(Math.random() * 30));
+            }
+            plots.push(points);
           }
-          plots.push(points);
-        }
-        return c3.generate({
-          bindto: '#schedule',
-          data: {
-            columns: plots,
-            type: 'area'
-          },
-          color: {
-            pattern: patts
-          },
-          axis: {
-            x: {
-              type: 'category',
-              label: {
-                text: 'Time of Day',
-                position: 'outer-center'
-              },
-              categories: [
-                '12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM',
-                '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM',
-                '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'
-              ]
+          return c3.generate({
+            bindto: '#schedule',
+            data: {
+              columns: plots,
+              type: 'step'
             },
-            y: {
-              label: {
-                text: 'Commits per day',
-                position: 'outer-middle'
+            color: {
+              pattern: patts
+            },
+            axis: {
+              x: {
+                type: 'category',
+                label: {
+                  text: 'Time of Day',
+                  position: 'outer-center'
+                },
+                categories: [
+                  '12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM',
+                  '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM',
+                  '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'
+                ]
+              },
+              y: {
+                label: {
+                  text: 'Commits per day',
+                  position: 'outer-middle'
+                }
               }
             }
-          },
-          tooltip: {
-            // show: false
-          }
+          });
         });
       }
     }
