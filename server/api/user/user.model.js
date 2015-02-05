9c5ca894a7b06155c6374005cf34b54a76575a51
Michael Staub <mikestaub22@gmail.com>
2015-02-04 19:42:15 -0800

    added a method on user model to return all git commits for today

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -2,9 +2,9 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var crypto = require('crypto');
-var authTypes = ['github'];
 var github = require('octonode');
+var moment = require('moment');
+var config = require('../../config');
 
 var UserSchema = new Schema({
   name: String,
@@ -19,5 +19,30 @@ var UserSchema = new Schema({
   fitbit: {},
   critter: {}
 });
+
+UserSchema.methods = {
+  getTodaysCommits: function(username){
+    var githubClient = github.client(config.cronToken);
+
+    githubClient.get('/users/'+username+'/events', {}, function(err, status, events, headers){
+      var todaysCommits = [];
+      for(var i = 0; i < events.length; i++){
+        var e = events[i];
+        if(e.type === 'PushEvent'){
+          var timestamp = moment(e.created_at, moment.ISO_8601);
+          var midnight = moment().startOf('day');
+          if(timestamp.isAfter(midnight)){
+            var pushEvent = {};
+            pushEvent.hour = timestamp.get('hour');
+            pushEvent.numCommits = e.payload.size;
+            pushEvent.repoName = e.repo.name;
+            todaysCommits.push(pushEvent);
+          }
+        }
+      }
+      return todaysCommits;
+    });
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema);
